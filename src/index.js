91d0ab2c057559d3a3f56e25d1d51888f71bdbf3
fatemehmi <fatemehmi2003@gmail.com>
2024-02-08 00:27:47 +0330

    update login

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./Styles/index.css";
 import App from "./App";
+import Login from "./Pages/Login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<Login className="normal-case" />
 	</React.StrictMode>
 );
